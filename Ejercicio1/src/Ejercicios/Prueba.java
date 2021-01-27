cdaacc5d1cea8c51f9df4d2c35b6e10c0963b75a
Dr4u95 <49389832+Dr4u95@users.noreply.github.com>
2021-01-27 08:48:48 +0100

    prueba3

--- Ejercicio1/src/Ejercicios/Prueba.java
+++ Ejercicio1/src/Ejercicios/Prueba.java
@@ -5,6 +5,7 @@ public class Prueba {
 	public static void main(String[] args) {
 		System.out.println("Hola");
 		System.out.println("Carmen San diego");
+		System.out.println("Ahora todo tiene sentido");
 
 	}
 
